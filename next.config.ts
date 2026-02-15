25ec30f8bc56d6c659e9b6a09dc0653c5d6dcd03
aftabmuddassir <aftabmuddassir147@gmail.com>
2026-02-15 17:51:11 -0500

    fix: image upload issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/dwjhoilfe/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
